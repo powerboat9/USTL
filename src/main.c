a87a403c8d8f2909eb1188451baa5d997a269005
powerboat9 <powerboat9.gamer@gmail.com>
2017-04-11 20:46:44 -0400

    Did a bit of work

--- src/main.c
+++ src/main.c
@@ -1,25 +1,37 @@
 #include <stdio.h>
 #include "portaudio.h"
 #define SAMPLE_RATE (96000)
-#typedef 
 
-static int callback(const void *inputBuffer, void *outputBuffer,
+#typedef struct {
+    float *sine;
+    unsigned int sineSize;
+    char phase;
+} SinGenData;
+
+PaStream *stream = NULL;
+
+static int sinCallback(const void *inputBuffer, void *outputBuffer,
                     unsigned long framesPerBuffer,
                     const PaStreamCallbackTimeInfo *timeInfo,
                     PaStreamCallbackFlags statusFlags,
                     void *userData) {
+    SinGenData data = (SinGenData) userData;
     
+}
 
-int start(int argn, char **args) {
-    PaStream *s
-    PaError err;
-    err = Pa_Initialize();
-    if (err != paNoError) goto error;
-    err = Pa_OpenDefaultStream(s&, 0, 1, paFloat32, SAMPLE_RATE, 256,
-                               callback, )
-    if (err != paNoError) goto error;
-    
+int start() {
+    if (stream == NULL) {
+        PaError err;
+        err = Pa_Initialize();
+        if (err != paNoError) goto error;
+        SinGenData data;
+        data->phase = 0x00;
+        data->
+        err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, SAMPLE_RATE, 256, callback, &data);
+        if (err != paNoError) goto error;
+        return 0;
 error:
-    printf("Error: %s\n", Pa_GetErrorText(err))
-    return -1;
+        printf("Error: %s\n", Pa_GetErrorText(err));
+        return -1;
+    }
 }
