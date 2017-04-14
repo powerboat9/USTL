3a3404f1a016ad2b330119b5afc42eaa7e9cb1fc
powerboat9 <powerboat9.gamer@gmail.com>
2017-04-14 14:37:52 -0400

    Did a bit of work

--- src/main.c
+++ src/main.c
@@ -2,36 +2,62 @@
 #include "portaudio.h"
 #define SAMPLE_RATE (96000)
 
+#define MAX_REPEATS (16)
+#define MAX_
+
+#define OUTPUT_TYPE paFloat32
+
 #typedef struct {
-    float *sine;
-    unsigned int sineSize;
-    char phase;
+    unsigned int size;
+    unsigned OUTPUT_TYPE *lookup;
+} SinLookupTable
+
+#typedef struct {
+    SinLookupTable *lookup;
+    unsigned double maxSamples;
+    unsigned int sample;
+    unsigned int freq;
+    bool freeLookupTable;
 } SinGenData;
 
 PaStream *stream = NULL;
+bool isPortAudioInitialized = false;
+
+static SinGenData getSinGenData(const unsigned double freq,
+                                const SinLookupTable *lookup) {
+    SinGenData data; /* Shouldn't need to zero out, we set all properties */
+    data->maxSamples = SAMPLE_RATE / freq;
+    data->sample = 0;
+    data->freq = freq;
+    if (lookup == NULL) {
+        data->lookup = genLookupTable(freq);
+        data->freeLookupTable = true;
+    } else {
+        data->lookup = lookup;
+        data->freeLookupTable = false;
+    }
+    return data;
+}
 
 static int sinCallback(const void *inputBuffer, void *outputBuffer,
                     unsigned long framesPerBuffer,
                     const PaStreamCallbackTimeInfo *timeInfo,
                     PaStreamCallbackFlags statusFlags,
                     void *userData) {
-    SinGenData data = (SinGenData) userData;
+    SinGenData *data = (SinGenData *) userData;
     
 }
 
-int start() {
-    if (stream == NULL) {
-        PaError err;
-        err = Pa_Initialize();
-        if (err != paNoError) goto error;
-        SinGenData data;
-        data->phase = 0x00;
-        data->
-        err = Pa_OpenDefaultStream(&stream, 0, 1, paFloat32, SAMPLE_RATE, 256, callback, &data);
-        if (err != paNoError) goto error;
+int initialize() { /* HAS to check for PortAudio initialization first */
+    if (!isPortAudioInitialized) {
+        PaError err = Pa_Initialize();
+        if (err != paNoError) {
+            fprintf(stderr, "Could not initialize PortAudio: %s\n", Pa_GetErrorText(err));
+            return -1;
+        }
         return 0;
-error:
-        printf("Error: %s\n", Pa_GetErrorText(err));
-        return -1;
     }
+    return -2;
 }
+
+char 
