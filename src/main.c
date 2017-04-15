6a99980d2bf7d2e22bb17cf13c86de87ddd34fea
powerboat9 <powerboat9.gamer@gmail.com>
2017-04-15 16:58:55 -0400

    Did a bit of work

--- src/main.c
+++ src/main.c
@@ -1,63 +1,51 @@
 #include <stdio.h>
+#include <math.h>
 #include "portaudio.h"
+
 #define SAMPLE_RATE (96000)
 
-#define MAX_REPEATS (16)
-#define MAX_
+#define TRANSMIT_BUFFER 256
 
-#define OUTPUT_TYPE paFloat32
+struct ChannelDescriptor {
+    unsigned short base;
+    unsigned short binSize;
+    unsigned short binCount;
+};
 
-#typedef struct {
-    unsigned int size;
-    unsigned OUTPUT_TYPE *lookup;
-} SinLookupTable
+struct WireDescriptor {
+    struct ChannelDescriptor *channels;
+    unsigned char channelCount;
+};
 
-#typedef struct {
-    SinLookupTable *lookup;
-    unsigned double maxSamples;
-    unsigned int sample;
-    unsigned int freq;
-    bool freeLookupTable;
-} SinGenData;
+struct SoundUserData {
+    unsigned short currentFrequency
+};
 
 PaStream *stream = NULL;
 bool isPortAudioInitialized = false;
 
-static SinGenData getSinGenData(const unsigned double freq,
-                                const SinLookupTable *lookup) {
-    SinGenData data; /* Shouldn't need to zero out, we set all properties */
-    data->maxSamples = SAMPLE_RATE / freq;
-    data->sample = 0;
-    data->freq = freq;
-    if (lookup == NULL) {
-        data->lookup = genLookupTable(freq);
-        data->freeLookupTable = true;
-    } else {
-        data->lookup = lookup;
-        data->freeLookupTable = false;
-    }
-    return data;
-}
-
-static int sinCallback(const void *inputBuffer, void *outputBuffer,
-                    unsigned long framesPerBuffer,
-                    const PaStreamCallbackTimeInfo *timeInfo,
-                    PaStreamCallbackFlags statusFlags,
-                    void *userData) {
-    SinGenData *data = (SinGenData *) userData;
-    
-}
-
-int initialize() { /* HAS to check for PortAudio initialization first */
+static int initialize() { /* HAS to check for PortAudio initialization first */
     if (!isPortAudioInitialized) {
         PaError err = Pa_Initialize();
         if (err != paNoError) {
             fprintf(stderr, "Could not initialize PortAudio: %s\n", Pa_GetErrorText(err));
             return -1;
         }
+        isPortAudioInitialized = true;
         return 0;
     }
     return -2;
 }
 
-char 
+static int transmissionCallback(const void *inputBuffer, void *outputBuffer,
+                    unsigned long framesPerBuffer,
+                    const PaStreamCallbackTimeInfo *timeInfo,
+                    PaStreamCallbackFlags statusFlags,
+                    void *userData) {
+}
+
+char *msgs[TRANSMIT_BUFFER];
+
+int transmit(struct ChannelDescriptor channel, char *data, unsigned int length) {
+    
+}
